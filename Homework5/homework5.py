7b5d2d4aa0808ab3f8f1596d21dad5b695efc970
tmutengwa <43368777+tmutengwa@users.noreply.github.com>
2025-10-30 12:59:01 +1100

    Add files via upload

--- Homework5/homework5.py
+++ Homework5/homework5.py
@@ -1,1 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
 
+# Load the model and vectorizer
+with open("pipeline_v1.bin", "rb") as f_in:
+    dv, model = pickle.load(f_in)
+
+# Define the input data model
+class Client(BaseModel):
+    lead_source: str
+    number_of_courses_viewed: int
+    annual_income: float
+
+# Create the FastAPI app
+app = FastAPI()
+
+@app.post("/predict")
+def predict(client: Client):
+    record = client.model_dump()
+    X = dv.transform([record])
+    y_pred = model.predict_proba(X)[:, 1]
+    return {"prediction_probability": y_pred[0]}
